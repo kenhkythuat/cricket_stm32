fc7360d286a521babe8e97fbbe8c171f5440f200
Thanh Son <20thanhson02@gmail.com>
2024-08-21 15:00:02 +0700

    simcom

---
+++ Core/sim/sim.h
@@ -0,0 +1,13 @@
+/*
+ * sim.h
+ *
+ *  Created on: Aug 21, 2024
+ *      Author: admin
+ */
+
+#ifndef SIM_SIM_H_
+#define SIM_SIM_H_
+
+#include"main.h"
+void blink(int second);
+#endif /* SIM_SIM_H_ */
--- Core/simcom/simcom.c
+++
@@ -1,9 +0,0 @@
-/*
- * simcom.c
- *
- *  Created on: Aug 21, 2024
- *      Author: admin
- */
-#include"simcom.h"
-
-#include"stdio.h"
